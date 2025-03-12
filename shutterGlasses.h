f2322213e9eaaa0eed29556cde086f70b2df301e
bo3b <github@bo3b.net>
2025-03-12 06:26:23 -0700

    Remove circular header include.
    Accidental left over. Fixes a clang-tidy warning.

--- shutterGlasses.h
+++ shutterGlasses.h
@@ -39,8 +39,6 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-#include "shutterGlasses.h"
-
 #include <windows.h>
 #include <string>
 #include <vector>
